5ba44dc4c247b97a5aa59bbbc02b5a4133c258c4
Sebastian Roblez <roblez769@gmail.com>
2026-02-04 13:32:39 -0500

    feat: Update Pico y Placa rules to use integers for restricted digits

--- src/domain/picoPlacaRules.js
+++ src/domain/picoPlacaRules.js
@@ -1,9 +1,9 @@
 const rules = {
-    monday:    ['1', '2'],
-    tuesday:   ['3', '4'],
-    wednesday: ['5', '6'],
-    thursday:  ['7', '8'],
-    friday:    ['9', '0'],
+    monday:    [1, 2],
+    tuesday:   [3, 4],
+    wednesday: [5, 6],
+    thursday:  [7, 8],
+    friday:    [9, 0],
     saturday:  [],
     sunday:    []
 }
